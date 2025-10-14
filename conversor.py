4e576f2da606dbb1461935a22a22dcba60c3338a
iza-max <izzadora.ribeiro3@gmail.com>
2025-10-14 11:51:40 -0300

    Create conversor.py

---
+++ conversor.py
@@ -0,0 +1,7 @@
+celsius = float(input("digite a temperatura em graus celsius"))
+
+kelvis = celsius + 273.15
+print(f"Temperatura em Kelvin: {kelvis} K")
+
+fahrenheit = (celsius * 9/5) + 32
+print(f"Temperatura em Fahrenheit: {fahrenheit} °F")
